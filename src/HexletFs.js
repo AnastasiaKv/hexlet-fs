815da37dbabb4085fad2a72762a9feac70487b2d
AnastasiaKv <anastasia.kvartalna@gmail.com>
2022-05-13 11:03:20 +0300

    hexletFs edit

--- src/HexletFs.js
+++ src/HexletFs.js
@@ -13,29 +13,74 @@ export default class {
 
   statSync(filepath) {
     const current = this.findNode(filepath);
+    if (!current) {
+      return null;
+    }
     return current.getMeta().getStats();
   }
 
-  touchSync(filepath) {
-    const { dir, base } = path.parse(filepath);
-    return this.findNode(dir).addChild(base, new File(base));
+  mkdirSync(filepath) {
+    const current = this.findNode(filepath);
+    if (current) {
+      return false;
+    }
+    const { base, dir } = path.parse(filepath);
+    const parent = this.findNode(dir);
+    if (!parent || !parent.getMeta().isDirectory()) {
+      return false;
+    }
+    parent.addChild(base, new Dir(base));
+    return true;
   }
 
-  mkdirSync(filepath) {
-    const { dir, base } = path.parse(filepath);
-    return this.findNode(dir).addChild(base, new Dir(base));
+  mkdirpSync(filepath) {
+    const current = this.findNode(filepath);
+    if (!current) {
+      const { dir } = path.parse(filepath);
+      this.mkdirpSync(dir);
+    } else if (!current.getMeta().isDirectory()) {
+      return false;
+    }
+    return this.mkdirSync(filepath);
+  }
+
+  readdirSync(filepath) {
+    const current = this.findNode(filepath);
+    if (!current || !current.getMeta().isDirectory()) {
+      return false;
+    }
+    return current.getChildren()
+      .map((child) => child.getKey());
+  }
+
+  touchSync(filepath) {
+    const { base, dir } = path.parse(filepath);
+    const parent = this.findNode(dir);
+    if (!parent) {
+      return false;
+    }
+    if (!parent.getMeta().isDirectory()) {
+      return false;
+    }
+    parent.addChild(base, new File(base, ''));
+    return true;
+  }
+
+  rmdirSync(filepath) {
+    const { base } = path.parse(filepath);
+    const current = this.findNode(filepath);
+    if (!current) {
+      return false;
+    }
+    if (!current.getMeta().isDirectory() || current.hasChildren()) {
+      return false;
+    }
+    current.getParent().removeChild(base);
+    return true;
   }
 
   findNode(filepath) {
     const parts = getPathParts(filepath);
     return parts.length === 0 ? this.tree : this.tree.getDeepChild(parts);
-  }
-
-  isFile(filepath) {
-    return this.statSync(filepath).isFile();
-  }
-
-  isDirectory(filepath) {
-    return this.statSync(filepath).isDirectory();
   }
 }
